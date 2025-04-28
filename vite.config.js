1ec1e7e1a16b4149cf54a180f43fd53c1ab52219
Diallo <maimounatou2002@gmail.com>
2025-04-28 19:00:17 +0200

    Correction des erreurs

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  // Nouveaux paramètres critiques
+  base: './', // Chemin relatif pour les assets
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: './' // Assets dans la racine de dist/
+  },
+  preview: {
+    port: process.env.PORT || 4173 // Port compatible Render
   }
 })
